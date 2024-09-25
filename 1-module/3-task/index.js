941a6b80c926be0063fb83e4f1da2c8ce87efc4a
kirill1918 <kirillpetrik2335@gmail.com>
2024-09-25 18:52:03 +0300

    Update index.js
    3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
+ var str = 'вася'
 function ucFirst(str) {
-  // ваш код...
+    if (str.length == 0){
+        return str
+    }else if (str.length > 0) {
+    return str[0].toUpperCase()+str.slice(1)
+    }
+        
+
 }
+console.log(ucFirst(str))
