cd17e3327f591c13b9913aba22fef277a13d19db
kirill1918 <kirillpetrik2335@gmail.com>
2024-09-26 16:51:59 +0300

    Update index.js
    4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,15 @@
-function checkSpam(str) {
-  // ваш код...
+function checkspam(str) {
+    const peq = str.toLowerCase()
+    if (peq.includes  ('1xbet') || peq.includes  ('xxx') || peq == ('1xbet') || peq == ('xxx'))  {
+        return true
+    }else{
+        return false
+    }
+
 }
+
+
+
+console.log(checkspam('innocent rabbit'));
+console.log(checkspam('1XbeT now'));
+console.log(checkspam('free xxxxx'));
