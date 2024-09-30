3efe0d9cb84c62a4ec75585c0935945acf5008b8
kirill1918 <kirillpetrik2335@gmail.com>
2024-10-01 02:24:31 +0300

    Update index.js
    5

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
-function factorial(n) {
-  // ваш код...
+function factorial(n) { 
+    let result = 1
+    while(n > 1) {
+        result = n * result
+        n = n - 1
+    }
+    return result
+if(n === 0) {
+    return 1
 }
+} 
+console.log(factorial(5))
