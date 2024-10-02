e30a7d867cf3bdb0b65e7ebd561e21366047e83b
kirill1918 <kirillpetrik2335@gmail.com>
2024-10-02 04:12:48 +0300

    Update index.js
    2.1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(salaries){
+    let sum = 0
+    for(let s1 in salaries){
+    if ( typeof salaries[s1] === 'number' && !isNaN (salaries[s1]) && salaries[s1] !== Infinity && salaries[s1] !== -Infinity){
+         sum += salaries[s1]
+    }
+    }
+    return sum
 }
+console.log(sumSalary(salaries))
