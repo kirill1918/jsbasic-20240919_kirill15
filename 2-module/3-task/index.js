20842a4409ffb390714be0403b384a8c7a765b3c
kirill1918 <kirillpetrik2335@gmail.com>
2024-10-07 20:35:30 +0300

    Update index.js
    2.3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
-let calculator = {
-  // ваш код
+let calculator  = {
+    sum() {
+        return Number(this.a) + Number(this.b);
+    },
+    mul() {
+        return Number(this.a) * Number(this.b);
+    },
+    read(a, b) {
+        this.a = a;
+        this.b = b;
+    }
 };
+calculator.read();
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
