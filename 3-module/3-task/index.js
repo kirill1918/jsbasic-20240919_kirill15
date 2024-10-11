7c7196d78bf1a5ecd8a9883043ced1a1d4ef7f83
kirill1918 <kirillpetrik2335@gmail.com>
2024-10-11 05:41:51 +0300

    Update index.js
    3.3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
+str = 'background-color'
 function camelize(str) {
-  // ваш код...
+let str1 = str.split('-') 
+return str1[0]+str1.slice(1).map(item => item[0].toUpperCase()+ item.slice(1)).join('');
+
+
 }
