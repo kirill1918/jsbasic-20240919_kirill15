93748831d9311e0ca86eeaa1c507fdcf97336a70
kirill1918 <kirillpetrik2335@gmail.com>
2024-10-11 05:30:49 +0300

    Update index.js
    3.5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,24 @@
+let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+let str = inputData;
+
 function getMinMax(str) {
-  // ваш код...
+    let str2 = str.split(' ');
+    let nu2 = [];
+
+    for (let index in str2) {
+        let num = Number(str2[index]); 
+        if (!isNaN(num)) { 
+            nu2.push(num); 
+        }
+    }
+
+    
+    let min1 = Math.min(...nu2);
+    let max1 = Math.max(...nu2);
+
+    
+    return { min: min1, max: max1 };
 }
+
+let result = getMinMax(str);
+console.log(result.min, result.max); 
