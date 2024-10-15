ad839b7b09674579cf2e54eeb64bd4a66f9542a1
kirill1918 <kirillpetrik2335@gmail.com>
2024-10-15 06:49:45 +0300

    Update index.js

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,8 @@
-function makeDiagonalRed(table) {
-  // ваш код...
-}
+ function makeDiagonalRed(table){
+            for (let i = 0; i < table.rows.length; i++){
+                let cell = table.rows[i].cells[i]; // .rows возвращает строку а .cell возвращает ячейку
+                cell.style.backgroundColor = ''
+
+            }
+        }
+        makeDiagonalRed(document.querySelector('table'))
