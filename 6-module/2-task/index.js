6ffe52770bc5a1d0a0a031dd78a82a1d98acf516
Your Name <you@example.com>
2026-05-14 13:10:44 +0300

    карточка

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,55 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
   constructor(product) {
+    this.product = product;
+    this.container = createElement(`<div class="card">
+      <div class="card__top">
+        <img src="/assets/images/products/${this.product.image}" class="card__image" alt="product">
+        <span class="card__price">€${this.product.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+        <div class="card__title">${this.product.name}</div>
+        <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    </div>`)
+
+    this.addEvent();
   }
+
+
+  
+  addEvent() { 
+    const button = this.container.querySelector('.card__button');
+    button.addEventListener('click' , () => { 
+      let ce = new CustomEvent('product-add' , { 
+        detail: this.product.id,
+        bubbles: true
+      })
+      button.dispatchEvent(ce)
+    })
+
+    button.addEventListener('product-add' , (event) => { 
+            console.log('Товар добавлен в корзину')
+        })
+  }
+ 
+
+  get elem() {
+    return this.container;
 }
+
+}
+
+
+  
+
+
+  
+  
+
+
+
+
