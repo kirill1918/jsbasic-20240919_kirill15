556b572f0c6957297e3e5888699ef8f5b35f1f6b
Your Name <you@example.com>
2026-05-14 15:39:42 +0300

    корзинка

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -42,12 +42,14 @@ export default class CartIcon {
     const icon = this.elem.querySelector('.cart-icon__inner');
     const cardContainer = document.querySelector('.container');
 
+    
+    if (!icon || !cardContainer) return;
+
     const pageWidth = window.innerWidth;
     const initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
 
     const iconPosition = cardContainer.getBoundingClientRect().right + 20;
     const iconLeftPosition = pageWidth - icon.offsetWidth - 10;
-    console.log('icon' , icon.offsetWidth)
     const leftIndent = Math.min(iconPosition, iconLeftPosition) + 'px';
 
     if (window.innerWidth <= 767) {
@@ -67,7 +69,7 @@ export default class CartIcon {
         position: 'fixed',
         top: '50px',
         left: leftIndent,
-        zIndex: 1e3,
+        zIndex: 1000,
         width: `${icon.offsetWidth}px`,
         height: `${icon.offsetHeight}px`
       });
@@ -81,5 +83,5 @@ export default class CartIcon {
         height: ''
       });
     }
-  }
+}
 }
