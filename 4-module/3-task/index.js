5bcb4fc5a1aa602d34057d5152f619e083e7f360
kirill1918 <kirillpetrik2335@gmail.com>
2026-05-15 20:09:54 +0300

    Update index.js

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,47 +1,37 @@
 function highlight(table) {
-  const tbody = table.querySelector('tbody');
-  const rows = tbody.rows;
+  const rows = table.tBodies[0].rows;
 
-  for (let i = 0; i < rows.length; i++) {
-    const row = rows[i];
+  for (let row of rows) {
     const statusCell = row.cells[3];
     const genderCell = row.cells[2];
     const ageCell = row.cells[1];
 
     
-    if (statusCell) {
-      if (statusCell.hasAttribute('data-available')) {
-        const availableValue = statusCell.getAttribute('data-available');
-
-        if (availableValue === 'true') {
-          statusCell.classList.add('available');
-        } else if (availableValue === 'false') {
-          statusCell.classList.add('unavailable');
-        }
-      } else {
-        
-        statusCell.hidden = true;
+    if (statusCell.hasAttribute('data-available')) {
+      const availableValue = statusCell.getAttribute('data-available');
+      if (availableValue === 'true') {
+        row.classList.add('available');
+      } else if (availableValue === 'false') {
+        row.classList.add('unavailable');
       }
+    } else {
+      row.hidden = true;
     }
 
     
-    if (genderCell) {
-      const genderText = genderCell.textContent.trim();
-      if (genderText === 'm') {
-        genderCell.classList.add('male');
-      } else if (genderText === 'f') {
-        genderCell.classList.add('female');
-      }
+    const genderText = genderCell.textContent.trim();
+    if (genderText === 'm') {
+      row.classList.add('male');
+    } else if (genderText === 'f') {
+      row.classList.add('female');
     }
 
     
-    if (ageCell) {
-      const age = Number(ageCell.textContent);
-
-      if (age < 18) {
-        ageCell.style.textDecoration = 'line-through';
-      }
+    const age = Number(ageCell.textContent);
+    if (age < 18) {
+      row.style.textDecoration = 'line-through';
     }
   }
 }
+
 
