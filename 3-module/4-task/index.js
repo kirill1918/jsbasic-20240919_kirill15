4441c022ce43a432ccdfaf0c142d311e05fc169f
kirill1918 <kirillpetrik2335@gmail.com>
2026-05-15 19:46:51 +0300

    Update index.js

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,27 +1,11 @@
-let user1 = {
-  "balance": "$1,825.65",
-  "picture": "https://placehold.it/32x32",
-  "age": 21,
-  "name": "Golden Branch",
-  "gender": "male",
-  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-};
-
-let users = [user1]
-
-
 function showSalary(users, age) {
-  for(let i = 0;  i < users.length; i++) { 
-    let user = users[i]
-    if(users.age <= age) { 
-       return `${user1.name} , ${user1.age}\n `
+  const result = [];
+  for (let i = 0;  i < users.length; i++) { 
+    let user = users[i];
+    if (user.age <= age) { 
+      result.push(`${user.name}, ${user.balance}`);
     }
 
   }
-
-  }
-
-let age = 50
-let result = showSalary(users, age);
-console.log(result)
+  return result.join('\n');
+}
