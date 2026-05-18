a01a2e1cde78dbc36eefb6ccbde21afab5587faf
Your Name <you@example.com>
2026-05-18 22:48:30 +0300

    задача

--- 9-module/2-task/index.js
+++ 9-module/2-task/index.js
@@ -5,6 +5,7 @@ import slides from '../../6-module/3-task/slides.js';
 import RibbonMenu from '../../7-module/1-task/index.js';
 import categories from '../../7-module/1-task/categories.js';
 
+
 import StepSlider from '../../7-module/4-task/index.js';
 import ProductsGrid from '../../8-module/2-task/index.js';
 
@@ -22,67 +23,55 @@ export default class Main {
     const holder = document.querySelector('[data-carousel-holder]');
     holder.appendChild(carousel.elem);
 
-
-
     const ribbonMenu = new RibbonMenu(this.categories);
     this.ribbonMenu = ribbonMenu;
     const ribbonHolder = document.querySelector('[data-ribbon-holder]');
     ribbonHolder.appendChild(ribbonMenu.elem);
 
-
-
-    const stepSlider = new StepSlider({ steps: 5, value: 3 });
+    
+    const stepSlider = new StepSlider({ steps: 5, value: 2 });
     this.stepSlider = stepSlider;
     const sliderHolder = document.querySelector('[data-slider-holder]');
     sliderHolder.appendChild(stepSlider.elem);
 
-
-
     const cartIcon = new CartIcon();
     const iconHolder = document.querySelector('[data-cart-icon-holder]');
-    iconHolder.appendChild(cartIcon.elem); 
+    iconHolder.appendChild(cartIcon.elem);
     this.cart = new Cart(cartIcon);
-
-
 
     const response = await fetch('products.json');
     const data = await response.json();
 
-
-
     this.productGrid = new ProductsGrid(data);
     const productHolder = document.querySelector('[data-products-grid-holder]');
+
+    
+    productHolder.innerHTML = '';
     productHolder.appendChild(this.productGrid.elem);
 
-   
-    this.productGrid.updateFilter({
-      noNuts: document.getElementById('nuts-checkbox').checked,
-      vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
-      maxSpiciness: this.stepSlider.value,
-      category: this.ribbonMenu.value
-});
-
-    document.body.addEventListener('product-add' , event => { 
-      const product = this.productGrid.products.find(el => el.id === event.detail)
-      if(product) { 
-        this.cart.addProduct(product)
+    document.body.addEventListener('product-add', event => {
+      const product = this.productGrid.products.find(el => el.id === event.detail);
+      if (product) {
+        this.cart.addProduct(product);
       }
-    })
+    });
 
-     this.stepSlider.elem.addEventListener('slider-change', () => {
+    
+    this.stepSlider.elem.addEventListener('slider-change', () => {
       this.productGrid.updateFilter({
         noNuts: document.getElementById('nuts-checkbox').checked,
         vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
-        maxSpiciness: this.stepSlider.value, 
+        maxSpiciness: this.stepSlider.value,
         category: this.ribbonMenu.value
       });
     });
 
+    
     this.ribbonMenu.elem.addEventListener('ribbon-select', (event) => {
       this.productGrid.updateFilter({
         noNuts: document.getElementById('nuts-checkbox').checked,
         vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
-        maxSpiciness: this.stepSlider.value, 
+        maxSpiciness: this.stepSlider.value,
         category: event.detail
       });
     });
@@ -92,19 +81,21 @@ export default class Main {
       const target = event.target.closest('.filters__checkbox');
       if (!target) return;
 
-  
-      const currentFilters = {
+      this.productGrid.updateFilter({
         noNuts: document.getElementById('nuts-checkbox').checked,
         vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
         maxSpiciness: this.stepSlider.value,
         category: this.ribbonMenu.value
-      };
-
-  
-      this.productGrid.updateFilter(currentFilters);
+      });
     });
 
     
+    this.productGrid.updateFilter({
+      noNuts: document.getElementById('nuts-checkbox').checked,
+      vegeterianOnly: document.getElementById('vegeterian-checkbox').checked,
+      maxSpiciness: this.stepSlider.value,
+      category: this.ribbonMenu.value
+    });
 
     return Promise.resolve();
   }
